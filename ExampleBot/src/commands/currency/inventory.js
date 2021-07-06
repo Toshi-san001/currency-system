36b26759dd068873de0a9f34106e202076cbb285
BIntelligent <65684589+BIntelligent@users.noreply.github.com>
2021-07-05 23:10:26 -0700

    Update inventory.js

--- ExampleBot/src/commands/currency/inventory.js
+++ ExampleBot/src/commands/currency/inventory.js
@@ -6,11 +6,13 @@ exports.run = async (client, message, args) => {
         user: message.author,
         guild: message.guild,
     });
-    let inv = result.inventory.slice(0,10)
+    let inv = result.inventory.slice(0, 10)
     const embed = new Discord.MessageEmbed()
-    .setDescription('Your Inventory!')
+        .setDescription('Your Inventory in Empty!')
     for (key of inv) {
-        embed.addField(`**${key.name}:**`, `Amount: ${key.amount}`)
+        embed.addField(`**${key.name}:**`, `Amount: ${key.amount}`);
+        embed.setDescription('Your Inventory!')
+
     }
     message.channel.send(embed)
 
